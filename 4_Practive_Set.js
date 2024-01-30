5e73974f258742af3d23a178be95c08c07b1cdca
jayad <jp@gmail.com>
2024-01-30 22:35:17 +0530

    Solve First Practice Set

---
+++ 4_Practive_Set.js
@@ -0,0 +1,29 @@
+// Eg.1
+let a = "Harry";
+let b = 5;
+console.log(a + b); //Harry5
+
+// Eg.2
+console.log(typeof (a + b)); //String
+
+// Eg.3
+const obj = {
+  name: "Hero",
+  section: 1,
+  isPrime: false,
+};
+// obj = 5555; //Not Allowed
+console.log(obj);
+
+// Eg.4
+obj["friend"] = "Robin";
+console.log(obj);
+
+// Eg.5
+const dict = {
+  appreciate: "recongnize",
+  atarixa: "state of freedom emotional",
+  yakka: "Hard Work",
+};
+
+console.log(dict);
