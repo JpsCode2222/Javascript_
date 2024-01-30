43acd7f493e3e7ca18a89397d0231bbebda30811
jayad <jp@gmail.com>
2024-01-30 22:01:23 +0530

    learn Variables declearation and its syntax

--- 1_Variables.js
+++ 1_Variables.js
@@ -1,14 +1,16 @@
 // variable = container to store a value;
-
-// Eg
-
 // variable Declearation
-var name = "James";
+// var , let , const
 
+// var : global scope
+var name = "James";
 var age = 10;
 
+// ES6 Feature : let , const
+// let : block scope {}
 let val = 11;
 
+// const : unique value
 const fullname = "James Bond";
 
 // to print use ->  console.log()
