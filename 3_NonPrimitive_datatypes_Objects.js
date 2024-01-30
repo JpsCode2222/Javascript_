14fbb31979109c98bd550674b6c0ecd1817794cd
jayad <jp@gmail.com>
2024-01-30 22:21:32 +0530

    Added Non Primitive Data type : Object

---
+++ 3_NonPrimitive_datatypes_Objects.js
@@ -0,0 +1,15 @@
+// Non Primitive datatype
+
+// Objects
+// key : pair data can be store in it
+const myObject = {
+  name: "James",
+  email: "j@gmail.com",
+  address: "demoAddress",
+  number: "546465464",
+  isActive: true,
+};
+
+// printing ways
+console.log(myObject.name);
+console.log(myObject["name"]);
