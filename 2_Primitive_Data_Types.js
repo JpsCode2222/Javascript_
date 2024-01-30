d79bac9532a4e5618407fa40073deb8a6192132d
jayad <jp@gmail.com>
2024-01-30 22:16:46 +0530

    Added Primitive Data types

---
+++ 2_Primitive_Data_Types.js
@@ -0,0 +1,17 @@
+// Primitive Data Types :
+// 7 types
+// NNSSBBU = null,number,symbol,string,boolean,bigint,undefined
+// datatype : which type of data store in variable
+
+let a = null;
+let b = 333;
+let c = true; // false
+let d = BigInt("333") + BigInt("3");
+let e = "John";
+let f = Symbol("I am nice symbol");
+let g = undefined;
+
+console.log(a, b, c, d, e, f, g);
+
+// check the type of variable
+console.log(typeof d);
