770b3303151b14a1b5949ff06f721847a38ece9c
jayad <jp@gmail.com>
2024-01-30 21:40:48 +0530

    first commit

---
+++ 1_Variables.js
@@ -0,0 +1,15 @@
+// variable = container to store a value;
+
+// Eg
+
+// variable Declearation
+var name = "James";
+
+var age = 10;
+
+let val = 11;
+
+const fullname = "James Bond";
+
+// to print use ->  console.log()
+console.log(name, age, val, fullname);
